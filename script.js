7ac173f45334d47088f49fe5258403809a9e4381
Caue Fortuna <caue.fortuna@interno.inflexion.com.br>
2018-01-23 22:46:57 -0200

    commit projeto2

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let titulo = document.querySelector('h1');
 let texto = document.querySelector('p');
+let tudo = document.querySelector('body');
+let teste = document.querySelector('.fotos');
+let b1 = document.querySelector('.banner');
 
 function mudarTitulo() {
   titulo.innerHTML = 'Sua cidade';
@@ -7,4 +10,17 @@ function mudarTitulo() {
 
 function mudarTexto() {
   texto.style.color = 'red';
+}
+
+function apagarLuz(){
+  tudo.style.backgroundColor = "#1b1c1c";
+}
+
+function inverterFotos(){
+  teste.style.flexDirection = "column";
+}
+
+function criarBorda(){
+  b1.style.border = "thick solid #0000FF";
+  teste.style.border = "thick solid orange";
 }
---
+++ teste.js
@@ -0,0 +1,5 @@
+let sub = document.querySelector('h2');
+
+function corTexto(){
+  sub.style.color = 'red';
+}
